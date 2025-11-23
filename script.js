0a6f4ec364aeccbcc70ed12a145500bf8b1bb31e
AmielAdriem29 <amieljpenaflor@gmail.com>
2025-11-23 22:41:09 +0800

    Complete js game

--- script.js
+++ script.js
@@ -2,14 +2,79 @@
 
 function getComputerChoice(){
     let number = (Math.ceil(Math.random()*3));
-    switch(number){
-        case 1:
-            return "rock";
-        case 2:
-            return "paper";
-        case 3:
-            return "scissors";
+    return number;
+}
+
+function getHumanChoice(){
+    let choice = String(prompt())
+    switch(choice){
+        case "rock":
+            return 1;
+        case "paper":
+            return 2;
+        case "scissors":
+            return 3;
     }
 }
 
-console.log(getComputerChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+function playRound(humanSelection = getHumanChoice(), computerSelection = getComputerChoice()){
+    let p1 = humanSelection
+    let p2 = computerSelection
+
+    //Rock vs Paper
+    if (p1 == 1 && p2 === 2) computerScore++;
+    if (p2 == 1 && p1 === 2) humanScore++;
+
+    //Paper vs Scissors
+    if (p1 == 2 && p2 === 3) computerScore++;
+    if (p2 == 2 && p1 === 3) humanScore++;
+
+    //Scissors vs Rock
+    if (p1 == 3 && p2 === 1) computerScore++;
+    if (p2 == 3 && p1 === 1) humanScore++;
+
+    switch(p1){
+        case 1:
+            console.log("Human: rock");
+            break;
+        case 2:
+            console.log("Human: paper");
+            break;
+        case 3:
+            console.log("Human: scissors");
+            break;
+    }
+
+    switch(p2){
+        case 1:
+            console.log("Computer: rock");
+            break;
+        case 2:
+            console.log("Computer: paper");
+            break;
+        case 3:
+            console.log("Computer: scissors");
+            break;
+    }
+
+    console.log("Human: "  + humanScore  + "\n" + "Computer: " + computerScore)
+
+}
+
+function playGame(){
+    for(i=1; i<=5; i++){
+        console.log("\nRound: " + i);
+        playRound()
+    }
+    if (humanScore > computerScore) console.log("Human Wins!")
+    else if (humanScore < computerScore) console.log("Computer Wins!")
+    else console.log("It's a tie!")
+}
+
+playGame()
